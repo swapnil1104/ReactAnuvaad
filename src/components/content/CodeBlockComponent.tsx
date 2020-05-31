1a3565803e4ad015ba77a33baaa5149d135f65da
Swapnil Tiwari <swapniltiwari775@gmail.com>
2020-05-31 16:03:46 +0530

    Code Refactoring and reorgansing -2

---
+++ src/components/content/CodeBlockComponent.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { androidstudio } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+interface CodeBlockProps {
+    code: string
+}
+
+export default class CodeBlockComponent extends React.Component<CodeBlockProps> {
+
+    render() {
+        return (
+            <div className="col-sm" style={{ width: 520, overflow: 'auto', textAlign: 'left' }}>
+                <SyntaxHighlighter language="xml" showLineNumbers={true} style={androidstudio} customStyle={{ height: 400 }}>
+                    {this.props.code}
+                </SyntaxHighlighter>
+            </div>
+        );
+    }
+
+}
