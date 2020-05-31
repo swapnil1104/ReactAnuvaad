d8a2c4306d955d4b77a590b324224619f23bdd6c
Swapnil Tiwari <swapniltiwari775@gmail.com>
2020-05-31 16:12:26 +0530

    Updates

---
+++ precache-manifest.5ece6d28a6f503e51c3738f80b89484a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc02c3a33c509e60c273a6ac4c90437a",
+    "url": "/ReactAnuvaad/index.html"
+  },
+  {
+    "revision": "86e0761be0aa65446c14",
+    "url": "/ReactAnuvaad/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "036cb925d300b842c5f2",
+    "url": "/ReactAnuvaad/static/js/2.39fb1635.chunk.js"
+  },
+  {
+    "revision": "41a6866a1b5bd1f04a40d009dc8f6e80",
+    "url": "/ReactAnuvaad/static/js/2.39fb1635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e0761be0aa65446c14",
+    "url": "/ReactAnuvaad/static/js/main.cd598295.chunk.js"
+  },
+  {
+    "revision": "3973161f71fa1bf75d9b",
+    "url": "/ReactAnuvaad/static/js/runtime-main.f121fb64.js"
+  }
+]);
