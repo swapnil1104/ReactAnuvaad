f916e10e191ba163fd067fea67f1d7e8cc91045d
Swapnil Tiwari <swapniltiwari775@gmail.com>
2020-07-04 13:07:03 +0530

    Updates

---
+++ precache-manifest.1e39f715b55e62e2089b49d6016528dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ff7fe1b25490758a078eb050c36929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edd9678ba5e77c307c31",
+    "url": "/static/css/main.2ce63322.chunk.css"
+  },
+  {
+    "revision": "e67f6b6de846aae59b03",
+    "url": "/static/js/2.a7022594.chunk.js"
+  },
+  {
+    "revision": "41a6866a1b5bd1f04a40d009dc8f6e80",
+    "url": "/static/js/2.a7022594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd9678ba5e77c307c31",
+    "url": "/static/js/main.4d5e2240.chunk.js"
+  },
+  {
+    "revision": "010b2a4ff3e7659884c6",
+    "url": "/static/js/runtime-main.1a5ba0d1.js"
+  }
+]);
